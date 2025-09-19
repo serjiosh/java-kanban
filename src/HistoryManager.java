9a951f48721d7413081964b7bcd72a3b8f7467a4
shalimovs <shalimovs@gmail.com>
2025-09-19 11:37:35 +0300

    Fix: Remove trailing whitespace in HistoryManager.java for CheckStyle compliance

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public interface HistoryManager {
     // Добавить задачу в историю просмотров
     void add(Task task);
-    
+
     // Удалить задачу из истории просмотров
     void remove(int id);
-    
+
     // Получить историю просмотров
     ArrayList<Task> getHistory();
 }
