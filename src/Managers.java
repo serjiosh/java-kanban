a1bc7c762524f264d965fb791c6008197a8fb10c
shalimovs <shalimovs@gmail.com>
2025-09-19 11:36:35 +0300

    Fix: Remove trailing whitespace in Managers.java for CheckStyle compliance

--- src/Managers.java
+++ src/Managers.java
@@ -1,10 +1,10 @@
 public class Managers {
-    
+
     // Создание менеджера задач по умолчанию
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
-    
+
     // Создание менеджера истории по умолчанию
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
